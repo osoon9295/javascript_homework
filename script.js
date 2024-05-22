ead62dc449a82d3242abb89349274cf62f73bfa3
Boyoung Lee <genuss040822@gamil.com>
2024-05-22 17:46:31 +0900

    8번 문제(로컬스토리지) 완료

---
+++ script.js
@@ -0,0 +1,21 @@
+// 문제 8번
+
+const user = {
+  이름: "병수",
+  나이: 20,
+  주소: "시골",
+};
+
+// 1.
+localStorage.setItem("user", JSON.stringify(user));
+// 2.
+console.log(JSON.parse(localStorage.getItem("user")));
+// 3.
+const user2 = JSON.parse(localStorage.getItem("user"));
+const newUser = { ...user2, 나이: 30 };
+// console.log(newAge);
+localStorage.setItem("user", JSON.stringify(newUser));
+
+// 4.
+localStorage.removeItem("user");
+console.log(JSON.parse(localStorage.getItem("user")));
